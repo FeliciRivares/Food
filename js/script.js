95e84175f1cd95f23a4c0c34ab627160fcaac2ac
Karina <karina.ostrovsika@gmail.com>
2022-11-08 00:45:11 +0200

    fixed card bug

--- js/script.js
+++ js/script.js
@@ -154,7 +154,7 @@ window.addEventListener('DOMContentLoaded', () => {
             }
          });
     // виклик модального вікна з плином часу 
-        //  const modalTimerId = setTimeout(openModal, 5000);
+          const modalTimerId = setTimeout(openModal, 5000);
     // виклик модального вікна якщо сторінка догорнута докінця 
         function showModalByScroll(){
             if (window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight -1){
@@ -170,12 +170,13 @@ window.addEventListener('DOMContentLoaded', () => {
 
         // класи карточок
         class MenuCard{
-            constructor(src, alt, title, descr ,price, parentSelector){
+            constructor(src, alt, title, descr ,price, parentSelector, ...classes){
                 this.src = src;
                 this.alt = alt;
                 this.title = title;
                 this.descr = descr;
                 this.price = price;
+                this.classes = classes;
                 this.parent = document.querySelector(parentSelector);
                 this.transfer = 27;
                 this.changeToUAH();
@@ -185,9 +186,17 @@ window.addEventListener('DOMContentLoaded', () => {
                 this.price = this.price * this.transfer;
             }
             render() {
+                // якщо не буде переданий клас встановлюється параметр за замовчуванням menu__item
                 const element = document.createElement('div');
+
+                if (this.classes.length === 0) { 
+                    this.element = 'menu__item';
+                    element.classList.add();
+                } else {
+                    this.classes.forEach(className => element.classList.add(className));
+                }
+                
                 element.innerHTML =`
-                <div class="menu__item">
                 <img src=${this.src} alt=${this.alt}>
                 <h3 class="menu__item-subtitle">${this.title}</h3>
                 <div class="menu__item-descr">${this.descr}</div>
@@ -196,7 +205,6 @@ window.addEventListener('DOMContentLoaded', () => {
                     <div class="menu__item-cost">Price:</div>
                     <div class="menu__item-total"><span>${this.price}</span> UAH/day</div>
                 </div>
-            </div>
                 `;
                 this.parent.append(element);
             }
@@ -208,7 +216,8 @@ window.addEventListener('DOMContentLoaded', () => {
             'Menu "Fitness"',
             'Menu "Fitness" is a new approach to cooking: more fresh vegetables and fruits. Product of active and healthy people. This is a brand new product with the best price and high quality!',
             10,
-            '.menu .container'
+            '.menu .container',
+            'menu__item'
         ).render();
         new MenuCard(
             "img/tabs/elite.jpg",
@@ -216,7 +225,8 @@ window.addEventListener('DOMContentLoaded', () => {
             'Menu “Premium”',
             'In the “Premium” menu, we use not only beautiful packaging design, but also high-quality execution of dishes. Red fish, seafood, fruits - a restaurant menu without going to a restaurant!',
             20,
-            '.menu .container'
+            '.menu .container',
+            'menu__item'
         ).render();
         new MenuCard(
             "img/tabs/post.jpg",
@@ -224,6 +234,7 @@ window.addEventListener('DOMContentLoaded', () => {
             '"Lenten" menu',
             'The Lenten menu is a careful selection of ingredients: the complete absence of animal products, milk from almonds, oats, coconut or buckwheat, the right amount of protein from tofu and imported vegetarian steaks.',
             15,
-            '.menu .container'
+            '.menu .container',
+            'menu__item'
         ).render();
 });
