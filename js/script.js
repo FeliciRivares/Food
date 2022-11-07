a920f72a1626cca86ffb6f4bdf7434563c5fc692
Karina <karina.ostrovsika@gmail.com>
2022-11-07 16:51:14 +0200

    modified cards for classes

--- js/script.js
+++ js/script.js
@@ -154,7 +154,7 @@ window.addEventListener('DOMContentLoaded', () => {
             }
          });
     // виклик модального вікна з плином часу 
-         const modalTimerId = setTimeout(openModal, 5000);
+        //  const modalTimerId = setTimeout(openModal, 5000);
     // виклик модального вікна якщо сторінка догорнута докінця 
         function showModalByScroll(){
             if (window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight -1){
@@ -168,5 +168,62 @@ window.addEventListener('DOMContentLoaded', () => {
 
 
 
-         
+        // класи карточок
+        class MenuCard{
+            constructor(src, alt, title, descr ,price, parentSelector){
+                this.src = src;
+                this.alt = alt;
+                this.title = title;
+                this.descr = descr;
+                this.price = price;
+                this.parent = document.querySelector(parentSelector);
+                this.transfer = 27;
+                this.changeToUAH();
+            }
+
+            changeToUAH() {
+                this.price = this.price * this.transfer;
+            }
+            render() {
+                const element = document.createElement('div');
+                element.innerHTML =`
+                <div class="menu__item">
+                <img src=${this.src} alt=${this.alt}>
+                <h3 class="menu__item-subtitle">${this.title}</h3>
+                <div class="menu__item-descr">${this.descr}</div>
+                <div class="menu__item-divider"></div>
+                <div class="menu__item-price">
+                    <div class="menu__item-cost">Price:</div>
+                    <div class="menu__item-total"><span>${this.price}</span> UAH/day</div>
+                </div>
+            </div>
+                `;
+                this.parent.append(element);
+            }
+        } 
+        // виклик обєкту лише один раз 
+        new MenuCard(
+            "img/tabs/vegy.jpg",
+            "vegy",
+            'Menu "Fitness"',
+            'Menu "Fitness" is a new approach to cooking: more fresh vegetables and fruits. Product of active and healthy people. This is a brand new product with the best price and high quality!',
+            10,
+            '.menu .container'
+        ).render();
+        new MenuCard(
+            "img/tabs/elite.jpg",
+            "elite",
+            'Menu “Premium”',
+            'In the “Premium” menu, we use not only beautiful packaging design, but also high-quality execution of dishes. Red fish, seafood, fruits - a restaurant menu without going to a restaurant!',
+            20,
+            '.menu .container'
+        ).render();
+        new MenuCard(
+            "img/tabs/post.jpg",
+            "post",
+            '"Lenten" menu',
+            'The Lenten menu is a careful selection of ingredients: the complete absence of animal products, milk from almonds, oats, coconut or buckwheat, the right amount of protein from tofu and imported vegetarian steaks.',
+            15,
+            '.menu .container'
+        ).render();
 });
