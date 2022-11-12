23117799e0ef43e7c47d591f2f5067d8c901ce2a
Karina <karina.ostrovsika@gmail.com>
2022-11-12 02:46:10 +0200

    added carousel slider

--- js/script.js
+++ js/script.js
@@ -346,45 +346,105 @@ window.addEventListener('DOMContentLoaded', () => {
          prev = document.querySelector('.offer__slider-prev'),
          next = document.querySelector('.offer__slider-next'),
          total =  document.querySelector('#total'),
-         current = document.querySelector('#current');
+         current = document.querySelector('#current'),
+         slidesWraper = document.querySelector('.offer__slider-wrapper'),
+         slidesField = document.querySelector('.offer__slider-inner'),
+         width = window.getComputedStyle(slidesWraper).width;
     let slideIndex = 1;
-        //  ініціалізуємо, починаємо роботу з 1 слайдера 
-    showSlides(slideIndex);
-     if(slides.length < 10){
-        total.textContent = `0${slides.length}`;
-     }else{
+    let offset= 0;
+// ------------------------------------------------------------------------ slider 2 start 
+    if(slides.length < 10){
+         total.textContent = `0${slides.length}`;
+         current.textContent =`0${slideIndex}`;
+    }else{
         total.textContent = slides.length;
-     }
-    function showSlides(n){
-        // перевірка на межі закіньчення слайдерів щоб йшло все по кругу  
-        if (n > slides.length){
-            slideIndex = 1;
-        }
-        if (n < 1){
-            slideIndex = slides.length;
-        }
-        // ховаємо всі слайдери залишаємо лише один 
-        slides.forEach(item => item.style.display = 'none');
-
-        slides[slideIndex - 1].style.display = 'block';
-
-        if(slides.length < 10){
-            current.textContent = `0${slideIndex}`;
-         }else{
-            current.textContent = slideIndex;
+        current.textContent = slideIndex;
          }
 
-      }
+         slidesField.style.width = 100 * slides.length + '%';
+         slidesField.style.display = 'flex';
+         slidesField.style.transition = '0.5s all';
 
-        function plusSlides(n){
-            showSlides(slideIndex += n);
-        }
+         slidesWraper.style.overflow = 'hidden';
 
-        prev.addEventListener('click', () =>{
-            plusSlides(-1);
-        });
-        next.addEventListener('click', () =>{
-            plusSlides(1);
-        });
+         slides.forEach(slide => {
+            slide.style.width = width;
+         });
+
+         next.addEventListener('click', () =>{
+            if (offset == +width.slice(0, width.length - 2) * (slides.length - 1)){
+                offset = 0;
+            } else {
+                offset += +width.slice(0, width.length - 2);
+            }
+            slidesField.style.transform = `translateX(-${offset}px)`;
+            if(slideIndex == slides.length){
+                slideIndex = 1;
+            }else{
+                slideIndex++;
+            }
+            if(slides.length < 10){
+                current.textContent = `0${slideIndex}`;
+            }else{
+                current.textContent = slideIndex;
+            }
+         });
+         prev.addEventListener('click', () =>{
+            if (offset == 0){
+                offset = +width.slice(0, width.length - 2) * (slides.length - 1);
+            } else {
+                offset -= +width.slice(0, width.length - 2);
+            }
+            slidesField.style.transform = `translateX(-${offset}px)`;
+            if(slideIndex == 1){
+                slideIndex = slides.length;
+            }else{
+                slideIndex--;
+            }
+            if(slides.length < 10){
+                current.textContent = `0${slideIndex}`;
+            }else{
+                current.textContent = slideIndex;
+            }
+         });
+         // -------------------------------------------------------------- slider 2 end
+        //  ініціалізуємо, починаємо роботу з 1 слайдера 
+    // showSlides(slideIndex);
+    //  if(slides.length < 10){
+    //     total.textContent = `0${slides.length}`;
+    //  }else{
+    //     total.textContent = slides.length;
+    //  }
+    // function showSlides(n){
+    //     // перевірка на межі закіньчення слайдерів щоб йшло все по кругу  
+    //     if (n > slides.length){
+    //         slideIndex = 1;
+    //     }
+    //     if (n < 1){
+    //         slideIndex = slides.length;
+    //     }
+    //     // ховаємо всі слайдери залишаємо лише один 
+    //     slides.forEach(item => item.style.display = 'none');
+
+    //     slides[slideIndex - 1].style.display = 'block';
+
+    //     if(slides.length < 10){
+    //         current.textContent = `0${slideIndex}`;
+    //      }else{
+    //         current.textContent = slideIndex;
+    //      }
+
+    //   }
+
+    //     function plusSlides(n){
+    //         showSlides(slideIndex += n);
+    //     }
+
+    //     prev.addEventListener('click', () =>{
+    //         plusSlides(-1);
+    //     });
+    //     next.addEventListener('click', () =>{
+    //         plusSlides(1);
+    //     });
 //  ------------------------  slider 1 - end
     });
