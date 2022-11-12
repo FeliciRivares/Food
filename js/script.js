7e6cdbeb6f325763d184dcab7f118a9ed2024036
Karina <karina.ostrovsika@gmail.com>
2022-11-12 02:09:20 +0200

    start working with slider

--- js/script.js
+++ js/script.js
@@ -341,7 +341,50 @@ window.addEventListener('DOMContentLoaded', () => {
                 closeModal();
             }, 4000);
          }
-        fetch('server.php')
-            .then(data => data.json())
-            .then(res => console.log(res));
+//  ------------------------  slider 1
+    const slides = document.querySelectorAll('.offer__slide'),
+         prev = document.querySelector('.offer__slider-prev'),
+         next = document.querySelector('.offer__slider-next'),
+         total =  document.querySelector('#total'),
+         current = document.querySelector('#current');
+    let slideIndex = 1;
+        //  ініціалізуємо, починаємо роботу з 1 слайдера 
+    showSlides(slideIndex);
+     if(slides.length < 10){
+        total.textContent = `0${slides.length}`;
+     }else{
+        total.textContent = slides.length;
+     }
+    function showSlides(n){
+        // перевірка на межі закіньчення слайдерів щоб йшло все по кругу  
+        if (n > slides.length){
+            slideIndex = 1;
+        }
+        if (n < 1){
+            slideIndex = slides.length;
+        }
+        // ховаємо всі слайдери залишаємо лише один 
+        slides.forEach(item => item.style.display = 'none');
+
+        slides[slideIndex - 1].style.display = 'block';
+
+        if(slides.length < 10){
+            current.textContent = `0${slideIndex}`;
+         }else{
+            current.textContent = slideIndex;
+         }
+
+      }
+
+        function plusSlides(n){
+            showSlides(slideIndex += n);
+        }
+
+        prev.addEventListener('click', () =>{
+            plusSlides(-1);
+        });
+        next.addEventListener('click', () =>{
+            plusSlides(1);
+        });
+//  ------------------------  slider 1 - end
     });
