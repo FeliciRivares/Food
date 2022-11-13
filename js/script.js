3c836ba61322d582f2fa6c5bd31a7951e91c1092
Karina <karina.ostrovsika@gmail.com>
2022-11-13 20:49:16 +0200

    added dots on the slider

--- js/script.js
+++ js/script.js
@@ -343,6 +343,7 @@ window.addEventListener('DOMContentLoaded', () => {
          }
 //  ------------------------  slider 1
     const slides = document.querySelectorAll('.offer__slide'),
+         slider  = document.querySelector('.offer__slider'),
          prev = document.querySelector('.offer__slider-prev'),
          next = document.querySelector('.offer__slider-next'),
          total =  document.querySelector('#total'),
@@ -371,11 +372,68 @@ window.addEventListener('DOMContentLoaded', () => {
             slide.style.width = width;
          });
 
+             // крапки на слайдерах 
+             slider.style.position =  'relative';
+             const indicators = document.createElement('ol'),
+                dots = [];
+             indicators.classList.add('carousel-indicators');
+             indicators.style.cssText =`
+                position: absolute;
+                right: 0;
+                bottom: 0;
+                left: 0;
+                z-index: 15;
+                display: flex;
+                justify-content: center;
+                margin-right: 15%;
+                margin-left: 15%;
+                list-style: none;
+             `;
+             slider.append(indicators);
+    
+             for(let i = 0; i < slides.length; i++){
+                const dot = document.createElement('li');
+                dot.setAttribute('data-slide-to', i + 1);
+                dot.style.cssText = `
+                    box-sizing: content-box;
+                    flex: 0 1 auto;
+                    width: 30px;
+                    height: 6px;
+                    margin-right: 3px;
+                    margin-left: 3px;
+                    cursor: pointer;
+                    background-color: #fff;
+                    background-clip: padding-box;
+                    border-top: 10px solid transparent;
+                    border-bottom: 10px solid transparent;
+                    opacity: .5;
+                    transition: opacity .6s ease;
+                `;
+                if (i == 0){
+                    dot.style.opacity = 1;
+                }
+                indicators.append(dot);
+                dots.push(dot);
+             }
+        function addZerov () {
+            if(slides.length < 10){
+                current.textContent = `0${slideIndex}`;
+            }else{
+                current.textContent = slideIndex;
+            }
+        }
+        function changeOpacity (){
+            dots.forEach(dot => dot.style.opacity = '.5');
+            dots[slideIndex - 1].style.opacity = '1';
+        }
+        function deleteNotDigits(text){
+            return +text.replace(/\D/g, ''); 
+        }
          next.addEventListener('click', () =>{
-            if (offset == +width.slice(0, width.length - 2) * (slides.length - 1)){
+            if (offset == deleteNotDigits(width) * (slides.length - 1)){
                 offset = 0;
             } else {
-                offset += +width.slice(0, width.length - 2);
+                offset += deleteNotDigits(width);
             }
             slidesField.style.transform = `translateX(-${offset}px)`;
             if(slideIndex == slides.length){
@@ -383,17 +441,15 @@ window.addEventListener('DOMContentLoaded', () => {
             }else{
                 slideIndex++;
             }
-            if(slides.length < 10){
-                current.textContent = `0${slideIndex}`;
-            }else{
-                current.textContent = slideIndex;
-            }
+            addZerov();
+            changeOpacity (); 
+
          });
          prev.addEventListener('click', () =>{
             if (offset == 0){
-                offset = +width.slice(0, width.length - 2) * (slides.length - 1);
+                offset = deleteNotDigits(width) * (slides.length - 1);
             } else {
-                offset -= +width.slice(0, width.length - 2);
+                offset -= deleteNotDigits(width);
             }
             slidesField.style.transform = `translateX(-${offset}px)`;
             if(slideIndex == 1){
@@ -401,11 +457,26 @@ window.addEventListener('DOMContentLoaded', () => {
             }else{
                 slideIndex--;
             }
-            if(slides.length < 10){
-                current.textContent = `0${slideIndex}`;
-            }else{
-                current.textContent = slideIndex;
-            }
+            addZerov();
+            changeOpacity ();
+
+         });
+
+         dots.forEach(dot =>{
+            dot.addEventListener('click', (e) =>{
+                const slideTo = e.target.getAttribute('data-slide-to');
+
+                slideIndex = slideTo;
+                offset = deleteNotDigits(width) * (slideTo - 1);
+
+                slidesField.style.transform = `translateX(-${offset}px)`;
+
+              
+                addZerov();
+                changeOpacity ();
+
+
+            });
          });
          // -------------------------------------------------------------- slider 2 end
         //  ініціалізуємо, починаємо роботу з 1 слайдера 
@@ -447,4 +518,6 @@ window.addEventListener('DOMContentLoaded', () => {
     //         plusSlides(1);
     //     });
 //  ------------------------  slider 1 - end
+
+
     });
